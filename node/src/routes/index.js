a7b874e6a549b5fd5101c592cef2d6ab7bbf8b6b
Edwin Tandiono <tandiono98@gmail.com>
2025-06-28 10:30:25 +0700

    refactor(web): Init revamp

--- node/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const salaryRecapRouter = require('./salary-recap');
-
-const router = express.Router();
-
-router.use('/salary-recap', salaryRecapRouter);
-
-module.exports = router;
